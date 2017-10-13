aa43edefbb171d0779d645fdd66ac87d232ab809
Tytus Planck <tytusplanck@gmail.com>
2017-10-13 09:49:44 -0400

    Added some actually stuff.

---
+++ Lab3/Assets/Scripts/Floor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour {
+ 
+     public Terrain terrain;
+     int terrainX; 
+     int terrainY;
+ 
+     void Awake(){
+         terrainX = terrain.terrainData.heightmapWidth;
+         terrainY = terrain.terrainData.heightmapHeight;
+         var heights = terrain.terrainData.GetHeights(0, 0, terrainX, terrainY);
+ 
+         for (int x = 0; x < terrainX; x++) {
+             for (int y = 0; y < terrainY; y++) {
+                 heights[x,y] = 0;
+             }
+         }
+         terrain.terrainData.SetHeights(0, 0, heights);
+     }
+ }
