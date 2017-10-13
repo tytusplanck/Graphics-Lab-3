753c5db3ae90e79006c5da780a9963c5bd511ba1
Kyle Rossman <kylerossman@Kyles-MBP.watchtv.net>
2017-10-13 16:29:32 -0400

    Trying to get particle to work

---
+++ Lab3/Assets/Scripts/Particle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle : MonoBehaviour {
+
+	public Vector3 velocity = new Vector3(1, 1, 0);
+	public GameObject particle;
+	float gravity = 9.8f;
+
+	// Use this for initialization
+	void Start () {
+		GameObject ball = Instantiate(particle);
+		ball.transform.parent = transform;
+		ball.transform.localPosition = new Vector3(0, 1, 0);
+	}
+	    
+	// Update is called once per frame
+	void Update () {
+	
+		velocity.y -= gravity * Time.deltaTime;
+		transform.position += velocity * Time.deltaTime;
+		        
+	}
+
+}
--- Lab3/Assets/Scripts/ParticleSceneManager.cs
+++ Lab3/Assets/Scripts/ParticleSceneManager.cs
@@ -6,9 +6,12 @@ public class ParticleSceneManager : MonoBehaviour {
 
 	public Floor floorPrefab;
 	private Floor floor;
+	public Particle particlePrefab;
+	private Particle ball;
 	// Use this for initialization
 	void Start () {
 		floor = Instantiate(floorPrefab) as Floor;
+		ball = Instantiate(particlePrefab) as Particle;
 	}
 	
 	// Update is called once per frame
