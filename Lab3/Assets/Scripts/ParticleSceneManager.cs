5bfb518a0a96d55bb26df30eefdccab5863a8d6a
Tytus Planck <tytusplanck@gmail.com>
2017-10-13 11:44:47 -0400

    Readded with git ignore.

---
+++ Lab3/Assets/Scripts/ParticleSceneManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSceneManager : MonoBehaviour {
+
+	public Floor floorPrefab;
+	private Floor floor;
+	// Use this for initialization
+	void Start () {
+		floor = Instantiate(floorPrefab) as Floor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
